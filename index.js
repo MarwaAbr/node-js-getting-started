79f91a799eefab9d2ffac1d12109949831a35e50
MarwaAbr <79973721+MarwaAbr@users.noreply.github.com>
2022-02-15 18:59:29 +0100

    code correction
    code correction

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
-cont {Pool} =require('pg')
+const {Pool} =require('pg')
 const PORT = process.env.PORT || 5000
 
 const DATABASE_URL= process.env.DATABASE_URL
-cont db = new Pool ({
+const db = new Pool ({
   connectionString: DATABASE_URL, ssl:{rejectUnauthorized:false}
 })
 
@@ -13,6 +13,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-   .get ('/api/contacts',async(req, res) => {
-const {rows}= await db.query( `SELECT name, email, phone FROM salesforce.contact`)res.json(rows)})
+  .get ('/api/contacts',async(req, res) => {
+      const {rows}= await db.query( `SELECT name, email, phone FROM salesforce.contact`)res.json(rows)})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
