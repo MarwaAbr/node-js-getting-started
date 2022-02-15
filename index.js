74f88cf8b350f6a3ef1b8898d80beeed899c2131
MarwaAbr <79973721+MarwaAbr@users.noreply.github.com>
2022-02-15 19:31:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const {Pool} =require('pg')
+const { Pool } =require('pg')
 const PORT = process.env.PORT || 5000
 
 const DATABASE_URL= process.env.DATABASE_URL
@@ -14,5 +14,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get ('/api/contacts',async(req, res) => {
-      const {rows}= await db.query( `SELECT name, email, phone FROM salesforce.contact`)res.json(rows)})
+      const {rows} = await db.query(`SELECT name, email, phone FROM salesforce.contact`)res.json(rows)})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
